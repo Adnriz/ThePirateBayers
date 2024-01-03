df54ee9cba9346e8fd0025ae172b0bf4ae0b2547
andersthomsen92 <doomknight92@gmail.com>
2024-01-03 21:52:27 +0100

    Basic GUI and FX::ID's

--- src/GUI/Controller/MainController.java
+++ src/GUI/Controller/MainController.java
@@ -1,4 +1,49 @@
 package GUI.Controller;
 
+import BE.Movie;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class MainController {
+
+    @FXML
+    private Button btnApplyFilters;
+
+    @FXML
+    private Button btnClearFilters;
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnNewMovie;
+
+    @FXML
+    private Button btnUpdate;
+
+    @FXML
+    private ComboBox<String> cbCategory1;
+
+    @FXML
+    private ComboBox<String> cbCategory2;
+
+    @FXML
+    private ComboBox<String> cbCategory3;
+
+    @FXML
+    private Spinner<Double> spinnerIMDB;
+
+    @FXML
+    private Spinner<Double> spinnerPersonal;
+
+    @FXML
+    private TableView<Movie> tblviewMovies;
+
+    @FXML
+    private TextField txtSearch;
+
 }
--- src/GUI/Controller/NewMovieController.java
+++ src/GUI/Controller/NewMovieController.java
@@ -1,4 +1,39 @@
 package GUI.Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+
 public class NewMovieController {
+
+    @FXML
+    private Button btnClose;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private ComboBox<?> cbCategory1;
+
+    @FXML
+    private ComboBox<?> cbCategory2;
+
+    @FXML
+    private ComboBox<?> cbCategory3;
+
+    @FXML
+    private DatePicker dateLastViewed;
+
+    @FXML
+    private Spinner<Double> spinnerIMDB;
+
+    @FXML
+    private Spinner<Double> spinnerPersonal;
+
+    @FXML
+    private TextField txtTitle;
+
 }
