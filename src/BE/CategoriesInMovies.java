7cd1b4d1c7b3201bba75ada9475fd3c9d2541734
Pumpozo <Adips4@hotmail.com>
2024-01-04 10:15:16 +0100

    Categor

--- src/BE/CategoriesInMovies.java
+++ src/BE/CategoriesInMovies.java
@@ -1,4 +1,34 @@
 package BE;
 
 public class CategoriesInMovies {
+
+    private int id;
+    private int categoryId;
+    private int movieId;
+
+    public CategoriesInMovies(int id, int categoryId, int movieId) {
+        this.id = id;
+        this.categoryId = categoryId;
+        this.movieId = movieId;
+    }
+
+    public CategoriesInMovies(int categoryId, int movieId) {
+        this.categoryId = categoryId;
+        this.movieId = movieId;
+    }
+
+    public CategoriesInMovies() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public int getMovieId() {
+        return movieId;
+    }
 }
--- src/BE/Category.java
+++ src/BE/Category.java
@@ -1,4 +1,27 @@
 package BE;
 
 public class Category {
+    private int id;
+    private String name;
+
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public Category() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 }
